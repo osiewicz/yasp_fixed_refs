58b1d2750a3b7912c70169435b80db3a7db1573e
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2020-04-13 14:13:36 +0200

    add methods for grouping notes by tags

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -4,9 +4,11 @@
       Note id:
       <input style="border: 1px solid black; margin: 5px" v-model="id" /><br />
       Edit content:
+      <input style="border: 1px solid black; margin: 5px" v-model="content" />
+      Edit tags:
       <input
         style="border: 1px solid black; margin: 5px"
-        v-model="content"
+        v-model="tags"
       /><br />
       <button
         style="border: 1px solid black; margin: 5px; padding: 5px"
@@ -19,6 +21,12 @@
         v-on:click="editNoteContent(id, content)"
       >
         Edit Note content
+      </button>
+      <button
+        style="border: 1px solid black; margin: 5px; padding: 5px"
+        v-on:click="editNoteTags(id, tags)"
+      >
+        Edit Note tags
       </button>
     </div>
     <button
@@ -90,6 +98,39 @@
         ><br /><br />
       </div>
     </div>
+    <div>
+      Group By Tags:
+      <input
+        style="border: 1px solid black; margin: 5px"
+        v-model="groupTags"
+      /><br />
+      <button
+        style="border: 1px solid black; margin: 5px; padding: 5px"
+        v-on:click="getNotesByTags(groupTags)"
+      >
+        Group by tags given (note must contain at least one tag given)</button
+      ><button
+        style="border: 1px solid black; margin: 5px; padding: 5px"
+        v-on:click="getNotesByTagsStrict(groupTags)"
+      >
+        Group by tags STRICT (note must contain all of tags given)</button
+      ><br />
+      <div v-for="note in groupResults" :key="note.id">
+        <span>Note</span><br />
+        <span>Id: {{ note.id }}</span
+        ><br />
+        <span>Title: {{ note.title }}</span
+        ><br />
+        <span>Content: {{ note.content }}</span
+        ><br />
+        <span>Tags: {{ note.tags }}</span
+        ><br />
+        <span>When created: {{ note.whenCreated }}</span
+        ><br />
+        <span>When edited: {{ note.whenEdited }}</span
+        ><br /><br />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -105,8 +146,11 @@ export default Vue.extend({
     return {
       id: "",
       content: "",
+      tags: "",
       searchPhrase: "",
-      searchResult: []
+      searchResult: [],
+      groupTags: "",
+      groupResults: []
     };
   },
   computed: {
@@ -117,7 +161,9 @@ export default Vue.extend({
       "getNoteById",
       "searchInContents",
       "searchInTitles",
-      "searchInContentsAndTitles"
+      "searchInContentsAndTitles",
+      "groupByTags",
+      "groupByTagsStrict"
     ])
   },
   methods: {
@@ -141,8 +187,21 @@ export default Vue.extend({
         this.commitEditNote(editableNote);
       } else console.log("There is no note with given id");
     },
+    editNoteTags(id: string, tags: string) {
+      const editableNote = this.getNoteById(id);
+      if (editableNote) {
+        editableNote.tags = tags.split(",");
+        this.commitEditNote(editableNote);
+      } else console.log("There is no note with given id");
+    },
     searchNotes(phrase: string) {
       this.searchResult = this.searchInContentsAndTitles(phrase);
+    },
+    getNotesByTags(tags: string) {
+      this.groupResults = this.groupByTags(tags.split(","));
+    },
+    getNotesByTagsStrict(tags: string) {
+      this.groupResults = this.groupByTagsStrict(tags.split(","));
     }
   }
 });
