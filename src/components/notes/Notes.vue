bc0ca5966f19890757d0f5c25d567dc96740371e
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2020-04-25 22:57:23 +0200

    finish routing

--- src/components/notes/Notes.vue
+++ src/components/notes/Notes.vue
@@ -2,59 +2,117 @@
   <v-layout>
     <v-container fluid grid-list-md>
       <v-layout row wrap>
-        <v-flex xs12 md6 lg3 v-for="(note, n) in notes" :key="n">
-          <v-card>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <v-list-item-title class="headline mb-1">{{
-                  note.title
-                }}</v-list-item-title>
-                <div
-                  style="whiteSpace: nowrap; overflow: hidden; textOverflow: ellipsis"
-                  class="overline mb-2"
-                >
-                  <span>Created: {{ note.whenCreated }} </span>
-                  <span>Edited: {{ note.whenEdited }}</span>
-                </div>
-                <div>
-                  <v-chip
-                    class="mb-3 ml-1"
-                    v-for="(tag, r) in getTwoTags(note.tags)"
-                    :key="'R' + r"
-                    color="orange"
+        <template v-if="!routeTag">
+          <v-flex xs12 md6 lg3 v-for="(note, n) in notes" :key="n">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{
+                    note.title
+                  }}</v-list-item-title>
+                  <div
+                    style="whiteSpace: nowrap; overflow: hidden; textOverflow: ellipsis"
+                    class="overline mb-2"
                   >
-                    &nbsp; <v-icon left>label</v-icon
-                    ><span
-                      style="max-width: 20ch; overflow: hidden; textOverflow: ellipsis"
-                      >{{ tag }}</span
+                    <span>Created: {{ note.whenCreated }} </span>
+                    <span>Edited: {{ note.whenEdited }}</span>
+                  </div>
+                  <div>
+                    <v-chip
+                      class="mb-3 ml-1"
+                      v-for="(tag, r) in getTwoTags(note.tags)"
+                      :key="'R' + r"
+                      color="orange"
                     >
-                  </v-chip>
-                  <v-chip
-                    v-if="note.tags.length > 3"
-                    class="mb-3 ml-1"
-                    color="orange"
+                      &nbsp; <v-icon left>label</v-icon
+                      ><span
+                        style="max-width: 10ch; overflow: hidden; textOverflow: ellipsis"
+                        >{{ tag }}</span
+                      >
+                    </v-chip>
+                    <v-chip
+                      v-if="note.tags.length > 3"
+                      class="mb-3 ml-1"
+                      color="orange"
+                    >
+                      &nbsp; <v-icon left>label</v-icon>
+                      {{ note.tags.length - 2 }} more
+                    </v-chip>
+                  </div>
+                  <v-list-item-subtitle
+                    style="-webkit-line-clamp: 3 !important"
+                    >{{ note.content }}</v-list-item-subtitle
                   >
-                    &nbsp; <v-icon left>label</v-icon>
-                    {{ note.tags.length - 2 }} more
-                  </v-chip>
-                </div>
-                <v-list-item-subtitle
-                  style="-webkit-line-clamp: 3 !important"
-                  >{{ note.content }}</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-actions>
-              <template>
-                <v-layout align-end justify-end>
-                  <v-btn text color="orange" @click.stop="viewClicked(note)"
-                    >View</v-btn
+                </v-list-item-content>
+              </v-list-item>
+              <v-card-actions>
+                <template>
+                  <v-layout align-end justify-end>
+                    <v-btn text color="orange" @click.stop="viewClicked(note)"
+                      >View</v-btn
+                    >
+                  </v-layout>
+                </template>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </template>
+        <template v-if="routeTag">
+          <v-flex
+            xs12
+            md6
+            lg3
+            v-for="(note, n) in groupByTags([routeTag])"
+            :key="n"
+          >
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{
+                    note.title
+                  }}</v-list-item-title>
+                  <div
+                    style="whiteSpace: nowrap; overflow: hidden; textOverflow: ellipsis"
+                    class="overline mb-2"
                   >
-                </v-layout>
-              </template>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
+                    <span>Created: {{ note.whenCreated }} </span>
+                    <span>Edited: {{ note.whenEdited }}</span>
+                  </div>
+                  <div>
+                    <v-chip class="mb-3 ml-1" color="orange">
+                      &nbsp; <v-icon left>label</v-icon
+                      ><span
+                        style="max-width: 10ch; overflow: hidden; textOverflow: ellipsis"
+                        >{{ routeTag }}</span
+                      >
+                    </v-chip>
+                    <v-chip
+                      v-if="note.tags.length >= 2"
+                      class="mb-3 ml-1"
+                      color="orange"
+                    >
+                      &nbsp; <v-icon left>label</v-icon>
+                      {{ note.tags.length - 1 }} more
+                    </v-chip>
+                  </div>
+                  <v-list-item-subtitle
+                    style="-webkit-line-clamp: 3 !important"
+                    >{{ note.content }}</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-card-actions>
+                <template>
+                  <v-layout align-end justify-end>
+                    <v-btn text color="orange" @click.stop="viewClicked(note)"
+                      >View</v-btn
+                    >
+                  </v-layout>
+                </template>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </template>
         <v-dialog persistent v-model="dialog" width="100vh">
           <v-card>
             <v-card-title class="headline pb-1">
@@ -124,13 +182,18 @@ export default Vue.extend({
     };
   },
   computed: {
-    ...mapGetters(["notes"])
+    ...mapGetters(["notes", "groupByTags"]),
+    routeTag() {
+      return this.$route.params.tag;
+    }
   },
   methods: {
     ...mapActions(["commitNote"]),
     viewClicked(note: Note) {
-      this.viewedNote = note;
-      this.dialog = true;
+      // eslint-disable-next-line
+      (this as any).viewedNote = note;
+      // eslint-disable-next-line
+      (this as any).dialog = true;
     },
     getTwoTags(tags: string[]) {
       if (tags.length > 3) {
